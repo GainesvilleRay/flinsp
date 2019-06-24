5a826f97e47a918ac8ba4bb1e3864db5fb94eb88
Doug Ray <doug.ray@gvillesun.com>
2019-06-24 15:55:26 -0400

    starting files

---
+++ districtlook.py
@@ -0,0 +1,35 @@
+# built-in libraries
+import csv
+import datetime
+import os.path
+import re
+import sys
+
+# installed with pip
+import pandas as pd
+
+# Create dataframe from file
+filepath = '1fdinspi.csv'
+colnames = [
+    "county", "licnum", "sitename", "streetaddy", "cityaddy", "zip",
+    "inspnum", "insptype", "inspdispos", "inspdate", "totalvio", "highvio",
+    "intermedvio", "basicvio", "vio22", "vio23", "vio24", "vio25", "vio26",
+    "vio28", "vio29", "vio30", "vio32", "vio33", "vio37", "vio43", "vio48",
+    "vio49", "vio50", "vio52", "vio56", "vio62", "vio71", "vio73", "vio74",
+    "licid", "visitid"
+    ]
+
+colnums = [
+    2, 4, 5, 6, 7, 8, 9, 12, 13, 14, 17, 18, 19, 20, 22, 23, 24, 25, 26,
+    28, 29, 30, 32, 33, 37, 43, 48, 49, 50, 52, 56, 62, 71, 73, 74, 80, 81
+    ]
+
+dist1_df = pd.read_csv(
+    filepath,
+    names=colnames,
+    usecols=colnums,
+    dtype=object,
+    encoding="ISO-8859-1"
+    )
+
+dist1_df.head()
